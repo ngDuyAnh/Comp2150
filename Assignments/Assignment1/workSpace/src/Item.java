9558aa0a88337e7076c5746820bd3eed8536c768
ngDuyAnh <In_Drop@outlook.com>
2021-01-31 12:41:40 -0600

    Assignment1. Setup class Item implements Printable for List.

---
+++ Assignments/Assignment1/workSpace/src/Item.java
@@ -0,0 +1,13 @@
+
+/*
+Duy Anh Nguyen 7892957
+January 31, 2021
+Item.java
+public abstract class Item implements Printable
+Parent call act as storage for item in list.
+*/
+
+public abstract class Item implements Printable
+{
+
+}
