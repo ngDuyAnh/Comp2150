93644571b550a0576b321cb724d93ff386292ae9
ngDuyAnh <In_Drop@outlook.com>
2021-01-31 16:10:31 -0600

    Assignment1. Done class User. Deleted class StringItem.

--- Assignments/Assignment1/workSpace/src/User.java
+++ Assignments/Assignment1/workSpace/src/User.java
@@ -11,27 +11,91 @@ There will be two public static methods. One public static
 All activities perform, request, by the user must be log
     and keep track.
 
-Private static member:
-USER_LIST_FILE - The file track of all registered users.
-USER_LOG_FILE_EXTENSION - The file extension name that log the
-    user's activities.
-
 Private member:
-userID - Unique user name.
-userLogFileName - The file name to the user activities log.
-
-Public static method:
-addNewUser() - Register new user.
-retrieveUser() - Retrieve registered user.
+userName - Unique user name.
+userLog - The file name to the user activities log.
 
 Public method:
-logActivity() - Log user's activity.
+User() - Constructor create a new instance of user.
+recordLog() - Log user's activity.
 userReport() - Return a string that is the user's information and
     activities. The order of the activities will be from oldest
     to newest.
 */
 
-public class User
+public class User extends Item
 {
+    // Private member
+    private final String userName; // The user name
+    private Log userLog = null;    // Log of user activities
+
+
+
+
+    // Public method
+
+    /* User()
+    Constructor create a new instance of user.
+    */
+    public User(final String USERNAME)
+    {
+        this.userName = USERNAME;
+        this.userLog = new Log();
+    }
+
+
+    /* recordLog()
+    Log the user's activity.
+
+    Parameter:
+    LOG - The LogPackage to log.
+    */
+    public void recordLog(final LogPackage LOG)
+    {
+        this.userLog.recordLog(LOG);
+    }
+
+
+    /* userReport()
+    Return a String that is the user's information and activities.
+    Format...
+        User name
+        { activities from oldest to newest }
+
+    Return:
+    A String that is all the user report information.
+    */
+    public String userReport()
+    {
+        // Local variable dictionary
+        String report = "";
+
+        // Get the user name
+        report += this.userName + "\n";
+
+        // Get the user activities
+        for (int counter = 0; counter < this.userLog.getLength(); counter++)
+        {
+            // Get the log package
+            final int TIME = this.userLog.readLogTime(counter);
+            final String COMMAND = this.userLog.readLogCommand(counter);
+
+            // Add to the report
+            report += TIME + " " + COMMAND + "\n";
+        }
+
+        // Return the user's report
+        return report;
+    }
+
+
+    /* print()
+    Print the user report.
+    */
+    @Override
+    public void print()
+    {
+        System.out.println(this.userReport());
+    }
 
 }
