924b3697ee5a6f5f75b94356a1d454e6dda9c04e
ngDuyAnh <In_Drop@outlook.com>
2021-03-02 03:45:28 +0000

    Assignment2. Node.h done.

---
+++ Assignments/Assignment2/workSpace/Node.h
@@ -0,0 +1,42 @@
+#pragma once
+
+/*
+Duy Anh Nguyen 7892957
+March 1, 2021
+Node.h
+class Node
+This will be the node for the linked list ADT.
+
+Private member:
+DATUM - Pointer to the datum the node will hold.
+nextNode - Pointer to the next node.
+prevNode - Pointer to the previous node.
+
+Public method:
+Node() - Constructor to create an instance of node.
+getDatum() - Return the datum the node holds.
+getNext() - Get the next node pointer.
+getPrev() - Get the previous node pointer.
+setNext() - Set the next node pointer.
+setPrev() - Set the previous node pointer.
+*/
+
+class ListItem;
+
+class Node {
+private:
+    // Private member
+    const ListItem* DATUM;    // Pointer to the datum
+    Node* nextNode = nullptr; // Pointer to the next node
+    Node* prevNode = nullptr; // Pointer to the previous node
+
+public:
+    Node(const ListItem* DATUM); // Constructor to create an instance of node
+    Node(const ListItem* DATUM, const Node* NEXT);
+    Node(const ListItem* DATUM, const Node* NEXT, const Node* PREV);
+    ListItem* getDatum(); // Get the datum the node holds
+    Node* getNext(); // Get the next node pointer
+    Node* getPrev(); // Get the previous node pointer
+    void setNext(const Node *NEXT); // Set the next pointer
+    void setPrev(const Node *PREV); // Set the previous pointer
+}; // class Node
