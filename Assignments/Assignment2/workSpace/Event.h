b3fb43a9871f1c3606206c2b03190380ad3d9ba1
ngDuyAnh <In_Drop@outlook.com>
2021-03-03 04:03:45 +0000

    Assignment2. Event.h done the layout of the class.

---
+++ Assignments/Assignment2/workSpace/Event.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "ListItem.h"
+
+/*
+Duy Anh Nguyen 7892957
+March 2, 2021
+Event.h
+class Event
+This represents an event waiting to be execute.
+
+Private member:
+eventTime - The time for the event to be execute.
+eventTag - The type of event.
+eventProcess - Pointer to the process.
+
+
+Public method:
+Event() - constructor to create an instance of Event.
+compareTo() - Compare the time of the current event to 
+        another ListItem.
+*/
+
+// Forward declaration
+class Process;
+
+class Event: public ListItem {
+private:
+    int eventTime = -1;           // The time for event to be execute
+    EventType eventTag = nullptr; // The type of event
+    Process *process = nullptr;   // Pointer to the process
+
+public:
+
+    // Public data type
+
+    /* enum EventType
+    Tag for the type of events.
+
+    Enumerator:
+    PROCESS_ARRIVAL - The arrival of a process waiting to be process.
+    PROCESS_EXIST - Done processing the process.
+    PROCESS_START_CPU - Process enter CPU processing.
+    PROCESS_COMPLETE_CPU - Process done CPU processing.
+    PROCESS_TIMEOUT_CPU - Process reaches the limit of CPU resource.
+    PROCESS_START_IO - Process enter IO processing.
+    PROCESS_COMPLETE_IO - Process done IO processing.
+    */
+    enum EventType
+    {
+        PROCESS_ARRIVAL = 0, 
+        PROCESS_EXIST, 
+        PROCESS_START_CPU, 
+        PROCESS_COMPLETE_CPU, 
+        PROCESS_TIMEOUT_CPU, 
+        PROCESS_START_IO, 
+        PROCESS_COMPLETE_IO
+    };
+
+    // Public method
+    Event(const int EVENT_TIME, const EventType EVENT_TAG, const Process* const PROCESS); // Constructor
+            // to create an instance of Event
+    int compareTo(const ListItem* const OTHER) override; // Compare the time of the current event to 
+                                                                // another ListItem
+};
