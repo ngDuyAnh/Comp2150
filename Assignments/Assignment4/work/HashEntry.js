49203c361da2abff644224d91ab46ab2c4467759
ngDuyAnh <In_Drop@outlook.com>
2021-04-13 17:20:13 -0500

    Assignment4. Done testing HashEntry.

--- Assignments/Assignment4/work/HashEntry.js
+++ Assignments/Assignment4/work/HashEntry.js
@@ -90,7 +90,7 @@ class HashEntry
     */
     get hashValue()
     {
-        this.#key.hashVal();
+        return this.#key.hashVal();
     }
 
 }
---
+++ Assignments/Assignment4/work/TestHashEntry.js
@@ -0,0 +1,66 @@
+let IntHash = require("./IntHash.js");
+let HashEntry = require("./HashEntry.js");
+
+// Global variable dictionary
+let test = null;
+
+
+// Test function
+
+/* testHashEntryConstructor()
+Test constructor.
+*/
+function testHashEntryConstructor()
+{
+    // Too many arguments
+    try
+    {
+        test = new HashEntry(new IntHash(0), 1, null);
+    }
+    catch (e)
+    {
+    }
+    console.assert(test === null, "Should not create an instance with too many arguments.");
+
+    // Too few arguments
+    try
+    {
+        test = new HashEntry(new IntHash(0));
+    }
+    catch (e)
+    {
+    }
+    console.assert(test === null, "Should not create an instance with too few arguments.");
+
+    // Invalid type
+    try
+    {
+        test = new HashEntry(0, 1);
+    }
+    catch (e)
+    {
+    }
+    console.assert(test === null, "Should not create an instance with invalid key.");
+
+    // Initialize with valid parameter
+    test = new HashEntry(new IntHash(0), 1);
+}
+
+
+// Main
+
+function main()
+{
+    // Constructor
+    testHashEntryConstructor();
+
+    // key
+    console.assert(test.key === 0, "The key should be 0.");
+
+    // value
+    console.assert(test.value === 1, "The value should be 1.");
+
+    // hashValue()
+    console.assert(test.hashValue === 0, "The hash value should be 0.");
+}
+main();
