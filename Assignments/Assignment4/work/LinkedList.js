126bc91ca667f9a10de74b419ff27d417f133ca0
ngDuyAnh <In_Drop@outlook.com>
2021-04-12 23:41:14 -0500

    Assignment4. Setup the Linked list and node. Done functionality comment. Next part is to implement.

---
+++ Assignments/Assignment4/work/LinkedList.js
@@ -0,0 +1,32 @@
+
+/*
+Duy Anh Nguyen 7892957
+April 12, 2021
+LinkedList.js
+class LinkedList
+Linked list abstract data type to hold instances of ListNode.
+
+Private member:
+#dummyHead - Pointer to the dummy node that is head of the linked list.
+#length - The number of data the linked list holds.
+
+Public method:
+constructor() - Constructor to create and initialize an instance.
+prepend() - Add the given datum to the front of the linked list.
+append() - Add the given datum to the end of the linked list.
+insertIndex() - Insert the given datum to the given index.
+removeHead() - Remove and return the first datum in the linked list.
+removeTail() - Remove and return the last datum in the linked list.
+removeIndex() - Remove and return the datum at given index.
+peekHead() - Get the first datum in the linked list but do not remove.
+peekTail() - Get the last datum in the linked list but do not remove.
+peekIndex() - Get the datum at given index in the linked list but do
+        not remove.
+isEmpty() - Return boolean flag if the linked list is empty.
+getLength() - Return the number of data the linked list holds.
+*/
+
+class LinkedList
+{
+
+}
---
+++ Assignments/Assignment4/work/ListNode.js
@@ -0,0 +1,26 @@
+
+/*
+Duy Anh Nguyen 7892957
+April 12, 2021
+ListNode.js
+class ListNode
+Container class to hold a datum in the linked list.
+
+Private member:
+#datum - Pointer to the datum.
+#nextNode - Pointer to the next node.
+#prevNode - Pointer to the previous node.
+
+Public method:
+constructor() - Constructor to create and initialize an instance.
+getDatum() - Getter method return datum.
+getNextNode() - Getter method return the next node.
+getPrevNode() - Getter method return the previous node.
+setNextNode() Setter method to set the next node field.
+setPrevNode() - Setter method to set the previous node field.
+*/
+
+class ListNode
+{
+
+}
