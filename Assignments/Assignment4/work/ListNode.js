96067b8b6bd57a28039dccd8be37f3c6b559bec1
ngDuyAnh <In_Drop@outlook.com>
2021-04-13 00:23:36 -0500

    Assignment4. ListNode done implementation. Ready for test.

--- Assignments/Assignment4/work/ListNode.js
+++ Assignments/Assignment4/work/ListNode.js
@@ -13,14 +13,131 @@ Private member:
 
 Public method:
 constructor() - Constructor to create and initialize an instance.
-getDatum() - Getter method return datum.
-getNextNode() - Getter method return the next node.
-getPrevNode() - Getter method return the previous node.
-setNextNode() Setter method to set the next node field.
+datum() - Getter method return datum.
+nextNode() - Getter method return the next node.
+prevNode() - Getter method return the previous node.
+setNextNode() - Setter method to set the next node field.
 setPrevNode() - Setter method to set the previous node field.
 */
 
 class ListNode
 {
+    // Private member
+    #datum = null;    // The datum
+    #nextNode = null; // Pointer to the next node
+    #prevNode = null; // Pointer to the previous node
 
+
+    // Public method
+
+    /* constructor()
+    Constructor to crate and initialize an instance.
+
+    Parameter:
+    DATUM - The datum the node holds.
+    NEXT - Pointer to the next node.
+    PREV - Pointer to the previous node.
+
+    Throws:
+    "Too many arguments" - Given more arguments than the method can take.
+    */
+    constructor(DATUM, NEXT, PREV)
+    {
+        if (arguments.length > 3)
+        {
+            throw "Too many arguments";
+        }
+        else
+        {
+            this.#datum = DATUM;
+            this.#nextNode = NEXT;
+            this.#prevNode = PREV;
+        }
+    }
+
+    /* getDatum()
+    Getter method return datum.
+
+    Return:
+    Return the datum the node holds.
+    */
+    get datum()
+    {
+        return this.#datum;
+    }
+
+    /* getNextNode()
+    Getter method return the next node.
+
+    Return:
+    Return the next node.
+    */
+    get nextNode()
+    {
+        return this.#nextNode;
+    }
+
+    /* prevNode()
+    Getter method return the previous node.
+
+    Return:
+    Return the previous node.
+    */
+    get prevNode()
+    {
+        return this.#prevNode;
+    }
+
+    /* setNextNode()
+    Setter method to set the next node field.
+
+    Parameter:
+    NEXT - Pointer to the next node.
+
+    Throws:
+    "Too many arguments" - Given more arguments than the method can take.
+    "Invalid type" - The parameter type is incorrect.
+    */
+    setNextNode(NEXT)
+    {
+        if (arguments > 1)
+        {
+            throw "Too many arguments";
+        }
+        else if (!(NEXT instanceof ListNode))
+        {
+            throw "Invalid type"
+        }
+        else
+        {
+            this.#nextNode = NEXT;
+        }
+    }
+
+    /* setPrevNode()
+    Setter method to set the previous node field.
+
+    Parameter:
+    PREV - Pointer to the next node.
+
+    Throws:
+    "Too many arguments" - Given more arguments than the method can take.
+    "Invalid type" - The parameter type is incorrect.
+    */
+    setNextNode(PREV)
+    {
+        if (arguments > 1)
+        {
+            throw "Too many arguments";
+        }
+        else if (!(PREV instanceof ListNode))
+        {
+            throw "Invalid type"
+        }
+        else
+        {
+            this.#nextNode = PREV;
+        }
+    }
+    
 }
