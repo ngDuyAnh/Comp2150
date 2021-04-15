d28ed2571e6bd76c04c86e16a6f712ec73a6bafc
ngDuyAnh <In_Drop@outlook.com>
2021-04-14 23:11:49 -0500

    Assignment4. Done the HuffmanEncode and main. Ready for final test.

---
+++ Assignments/Assignment4/work/HuffmanEncodeMain.js
@@ -0,0 +1,27 @@
+let readline = require("readline");
+let HuffmanEncode = require("./HuffmanEncode.js");
+
+/*
+Duy Anh Nguyen 7892957
+April 14, 2021
+HuffmanEncodeMain.js
+The main entry to the Huffman encode program.
+*/
+
+function main()
+{
+    // Get the input and output file name
+    let INPUT_FILE_NAME = "input.txt";
+    let OUTPUT_FILE_NAME = INPUT_FILE_NAME + ".huff";
+
+    // Encode the input file to output file
+    try
+    {
+        HuffmanEncode.encodeFile(INPUT_FILE_NAME, OUTPUT_FILE_NAME);
+    }
+    catch (e)
+    {
+        console.error(e);
+    }
+}
+main();
